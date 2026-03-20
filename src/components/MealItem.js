e1bda4be55c34291ffbab5df94a433e0800f2ce1
christopher12334 <Christopher.alapert@voco.ee>
2026-03-20 10:56:16 +0200

    Ülesanne 5 - Valuuta andmete kuvamine

--- src/components/MealItem.js
+++ src/components/MealItem.js
@@ -1,11 +1,18 @@
+const currencyFormatter = new Intl.NumberFormat('et-EE', {
+    style: 'currency',
+    currency: 'EUR',
+});
+
 const MealItem = ({ name, price, description, image }) => {
+    const formattedPrice = currencyFormatter.format(price);
+
     return (
         <li className="meal-item">
             <article>
                 <img src={require(`../assets/${image}`)} alt={name}/>
                 <div>
                     <h3>{name}</h3>
-                    <p className="meal-item-price">{price}</p>
+                    <p className="meal-item-price">{formattedPrice}</p>
                     <p className="meal-item-description">{description}</p>
                 </div>
                 <p className="meal-item-actions">
