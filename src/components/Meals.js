31c32e3bc82adc1d81f93ec7b98c0c569ee13e2d
christopher12334 <Christopher.alapert@voco.ee>
2026-03-20 10:46:00 +0200

    Ülesanne 3 - andmete pärimine Meals.js komponendis

--- src/components/Meals.js
+++ src/components/Meals.js
@@ -1,4 +1,16 @@
+import { useEffect } from 'react';
+
 const Meals = () => {
+    useEffect(() => {
+        const fetchMeals = async () => {
+            const response = await fetch('http://localhost:3001/meals');
+            const meals = await response.json();
+            console.log(meals);
+        };
+
+        fetchMeals();
+    }, []);
+
     return (
         <ul id="meals">
             <h2>create list of meals, using fetch data from backend</h2>
