591bc9bc1ddb494a361434805b64d6f7fd33f547
christopher12334 <Christopher.alapert@voco.ee>
2026-03-20 11:34:39 +0200

    Ülesanne 8 - useReducer hooki lisamine

--- src/App.js
+++ src/App.js
@@ -1,13 +1,57 @@
+import { useReducer } from 'react';
 import Header from './components/Header';
 import Meals from './components/Meals';
-import { CartContextProvider } from './store/CartContext';
+import CartContext from './store/CartContext';
+
+const defaultCartState = {
+  items: [],
+};
+
+const cartReducer = (state, action) => {
+  if (action.type === 'ADD_ITEM') {
+    const existingItemIndex = state.items.findIndex((item) => item.id === action.item.id);
+    const updatedItems = [...state.items];
+
+    if (existingItemIndex > -1) {
+      const existingItem = updatedItems[existingItemIndex];
+
+      updatedItems[existingItemIndex] = {
+        ...existingItem,
+        quantity: existingItem.quantity + 1,
+      };
+    } else {
+      updatedItems.push({
+        ...action.item,
+        quantity: 1,
+      });
+    }
+
+    return {
+      ...state,
+      items: updatedItems,
+    };
+  }
+
+  return defaultCartState;
+};
 
 const App = () => {
+  const [cartState, cartDispatch] = useReducer(cartReducer, defaultCartState);
+
+  const handleAddItemToCart = (item) => {
+    cartDispatch({ type: 'ADD_ITEM', item });
+  };
+
+  const cartContext = {
+    items: cartState.items,
+    addItem: handleAddItemToCart,
+  };
+
   return (
-    <CartContextProvider>
+    <CartContext.Provider value={cartContext}>
       <Header />
       <Meals />
-    </CartContextProvider>
+    </CartContext.Provider>
   );
 };
 
--- src/store/CartContext.js
+++ src/store/CartContext.js
@@ -1,39 +1,8 @@
-import { createContext, useState } from 'react';
+import { createContext } from 'react';
 
 const CartContext = createContext({
   items: [],
   addItem: () => {},
 });
-
-export const CartContextProvider = ({ children }) => {
-  const [items, setItems] = useState([]);
-
-  const addItem = (item) => {
-    setItems((prevItems) => {
-      const existingItemIndex = prevItems.findIndex((cartItem) => cartItem.id === item.id);
-
-      if (existingItemIndex === -1) {
-        return [...prevItems, { ...item, quantity: 1 }];
-      }
-
-      const updatedItems = [...prevItems];
-      const existingItem = updatedItems[existingItemIndex];
-
-      updatedItems[existingItemIndex] = {
-        ...existingItem,
-        quantity: existingItem.quantity + 1,
-      };
-
-      return updatedItems;
-    });
-  };
-
-  const cartContext = {
-    items,
-    addItem,
-  };
-
-  return <CartContext.Provider value={cartContext}>{children}</CartContext.Provider>;
-};
 
 export default CartContext;
