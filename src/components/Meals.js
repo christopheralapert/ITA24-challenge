75b328c2e357c43b353a2c99875e1f65e19d26cc
christopher12334 <Christopher.alapert@voco.ee>
2026-03-20 10:38:04 +0200

    Ülesanne 1 - Header and Meals komponentide lisamine rakendusele

--- src/components/Meals.js
+++ src/components/Meals.js
@@ -1,9 +1,7 @@
 const Meals = () => {
     return (
         <ul id="meals">
-            { 
-                // list of meals
-            }
+            <h2>create list of meals, using fetch data from backend</h2>
         </ul>
     )
 }
